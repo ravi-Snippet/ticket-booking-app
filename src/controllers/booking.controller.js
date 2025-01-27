221ee57b054b7a0b733f165cf219c55c00d97c17
ravi-Snippet <raviroshan1994@gmail.com>
2025-01-27 21:39:20 +0530

    fix: sample changes

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -3,7 +3,7 @@ async function getBookings(req, res) {
     try {
         return res.send({
             username: 'ravi',
-            bookings: ['booking1', 'booking2']
+            bookings: ['booking1', 'booking2', 'booking3']
         });
     } catch (error) {
         console.log('error :: ', error)
